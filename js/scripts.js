414bff5ac68477e36ea7f3c4e218cff241751464
Szymon <skowalski13th@gmail.com>
2022-05-11 16:23:55 +0200

    Daily update
    Added hamburger for mobile resolution

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 function toggleButton() {
-  document.getElementsByClassName("search-icon").style="'background-image: url(./img/mobile_menu_close)'"
-  document.getElementsByClassName("searchbar-hidden").class="searchbar"
+  document.getElementsByClassName("search-icon").style="background-image: url(./img/mobile_menu_close)";
+  document.getElementsByClassName("searchbar-hidden").class="searchbar";
 }
