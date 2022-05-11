9d8dc908ef9fc44c8839f95429533e7fa1177884
Szymon <skowalski13th@gmail.com>
2022-05-11 17:29:14 +0200

    Fuck JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
-function toggleButton() {
-  document.getElementsByClassName("search-icon").style="background-image: url(./img/mobile_menu_close)";
-  document.getElementsByClassName("searchbar-hidden").class="searchbar";
+function searchToggle() {
+  var x = document.getElementById("search");
+  if (x.style.display === "block") {
+    x.style.display = "none"; 
+  } else {
+    x.style.display = "block"
+  }
+}
+
+function iconSwap() {
+  var x = document.getElementById("searchIcon");
+  if (x.className === "search-icon") {
+    x.className = "close-icon";
+  } else {
+    x.className = "search-icon"
+  }
+}
+
+function miniIconSwap() {
+  var x = document.getElementById("searchIconMini");
+  if (x.className === "search-icon") {
+    x.className = "close-icon";
+  } else {
+    x.className = "search-icon"
+  }
 }
