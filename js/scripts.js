76c58f11adb35a0e982692e6107c8d2cde0e0130
Prophetter <babybeel69@gmail.com>
2022-05-12 02:57:41 +0200

    Sidebar for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,18 @@ function miniIconSwap() {
   if (x.className === "search-icon") {
     x.className = "close-icon";
   } else {
-    x.className = "search-icon"
+    x.className = "search-icon";
   }
+}
+
+function hamburger() {
+    var hamburger = document.querySelector(".hamburger");
+        hamburger.classList.toggle("is-active");
+}
+
+function sidebarToggle() {
+  var side = document.querySelector(".sidebar");
+  var main = document.querySelector(".main");
+      side.classList.toggle("sidebar-toggle");
+      main.classList.toggle("main-translate");
 }
