307e0417bbaed6df9c00177b64d93777cd26f9d7
Szymon <skowalski13th@gmail.com>
2022-05-23 10:30:45 +0200

    Almost ready

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,65 @@
 function searchToggle() {
-  var x = document.getElementById("search");
-  if (x.style.display === "block") {
-    x.style.display = "none"; 
-  } else {
-    x.style.display = "block"
-  }
+    var x = document.getElementById("search");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
 }
 
 function iconSwap() {
-  var x = document.getElementById("searchIcon");
-  if (x.className === "search-icon") {
-    x.className = "close-icon";
-  } else {
-    x.className = "search-icon"
-  }
+    var x = document.getElementById("searchIcon");
+    if (x.className === "search-icon") {
+        x.className = "close-icon";
+    } else {
+        x.className = "search-icon"
+    }
 }
 
 function miniIconSwap() {
-  var x = document.getElementById("searchIconMini");
-  if (x.className === "search-icon") {
-    x.className = "close-icon";
-  } else {
-    x.className = "search-icon";
-  }
+    var x = document.getElementById("searchIconMini");
+    if (x.className === "search-icon") {
+        x.className = "close-icon";
+    } else {
+        x.className = "search-icon";
+    }
 }
 
 function hamburger() {
     var hamburger = document.querySelector(".hamburger");
-        hamburger.classList.toggle("is-active");
+    hamburger.classList.toggle("is-active");
 }
 
 function sidebarToggle() {
-  var side = document.querySelector(".sidebar");
-  var main = document.querySelector(".main");
-      side.classList.toggle("sidebar-toggle");
-      main.classList.toggle("main-translate");
+    var side = document.querySelector(".sidebar-container");
+    var main = document.querySelector(".main");
+    main.classList.toggle("sidebar-open");
+    side.classList.toggle("sidebar-opened");
 }
 
-function sideHide() {
-  var x = document.getElementById("side-hide");
-  if (x.style.display === "block") {
-    x.style.display = "none"; 
-  } else {
-    x.style.display = "block"
-  }
-}
-
-function sidebarSwitch() {
-  var drop = document.getElementById("dropmenu")
-  if (drop.style.display === "block") {
-      drop.style.display = "none";
-  } else {
-      drop.style.display = "block";
-  }
-}
+// function sideHide() {
+//   var x = document.getElementById("side-hide");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block"
+//   }
+// }
+//
+// function sidebarSwitch() {
+//   var drop = document.getElementById("dropmenu")
+//   if (drop.style.display === "block") {
+//       drop.style.display = "none";
+//   } else {
+//       drop.style.display = "block";
+//   }
+// }
 
 function plusMinus() {
-  var plusminus = document.getElementById("side-icons");
-      if (plusminus.className === "fa-solid fa-minus") {
-          plusminus.className = "fa-solid fa-plus";
-      } else {
-          plusminus.className = "fa-solid fa-minus";
-      }
+    var plusminus = document.getElementById("side-icons");
+    if (plusminus.className === "fa-solid fa-minus") {
+        plusminus.className = "fa-solid fa-plus";
+    } else {
+        plusminus.className = "fa-solid fa-minus";
+    }
 }
